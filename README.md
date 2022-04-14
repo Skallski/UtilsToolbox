1066d910ce8956c0ff4a5f8aa2223e28830e8946
Skallu <patryk.kaminski0711@gmail.com>
2022-04-15 01:11:25 +0200

    changed namespaces, updated GameObject extensions, updated README.md

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # Skallu-Utils
-Unity package of tools and stuff, which I find useful
+My package of Unity [extensions](http://en.wikipedia.org/wiki/Extension_method)) and stuff, which I find useful.
+
+## Usage
+
+## Extensions
+
+All the extensions here are under development.
+
+```csharp
+using SkalluUtils.Extensions;
+```
+
+### GameObject
+
+```csharp
+// destroys all components of game object except the one provided as method parameter
+gameObject.DestroyComponentsExceptProvided(spriteRenderer);
+
+// destroys all components of game object except the list of those provided as method parameter
+gameObject.DestroyComponentsExceptProvided(new List<Component>{spriteRenderer, rigidBody2D});
+
+// destroys all components of game object
+gameObject.DestroyAllComponents();
+```
+
+### string
+
+```csharp
+// changes color of log message to one provided as method parameter
+Debug.Log("sample text".Color(Color.blue));
+```
+
+## Property Attributes
+
+```csharp
+using SkalluUtils.PropertyAttributes;
+```
+
+### Read Only Inspector
+
+`ReadOnlyInspector` is a custom property attribute, which makes serialized variables read-only
+
+```csharp
+using SkalluUtils.PropertyAttributes.ReadOnlyInspectorPropertyAttribute;
+```
+
+```csharp
+[ReadOnlyInspector] [SerializeField] private bool canAttack;
+```
+
+## Utils
+
+All the utils here are under development.
+
+```csharp
+using SkalluUtils.Utils;
+```
--- Runtime/Utility/Utils.cs
+++ Runtime/Utility/Utils.cs
@@ -1,13 +1,14 @@
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
+using System.Collections.Generic;
 using UnityEngine;
 
-namespace Skallu.Utils
+namespace SkalluUtils
 {
     namespace PropertyAttributes
     {
-        namespace ReadOnlyInspector
+        namespace ReadOnlyInspectorPropertyAttribute
         {
             [CustomPropertyDrawer(typeof(ReadOnlyInspectorAttribute))]
             public class ReadOnlyInspectorDrawer : PropertyDrawer
@@ -24,13 +25,21 @@ namespace Skallu.Utils
             public class ReadOnlyInspectorAttribute : PropertyAttribute {}
         }
     }
-    
-    namespace Tools
+
+    namespace Utils
     {
-        public class Utils : MonoBehaviour
+        public static class Utils
+        {
+            
+        }
+    }
+    
+    namespace Extensions
+    {
+        public static class GameObjectExtensionUtils
         {
             // destroys all components of game object except the one provided as method parameter
-            public static void DestroyComponentsExceptProvided(GameObject gameObject, Component componentToKeep)
+            public static void DestroyComponentsExceptProvided(this GameObject gameObject, Component componentToKeep)
             {
                 var allComponents = gameObject.GetComponents(typeof(Component));
 
@@ -39,13 +48,31 @@ namespace Skallu.Utils
                     if (component != null)
                     {
                         if (component.GetType() != componentToKeep.GetType() && component.GetType() != typeof(Transform)) // Transform component cannot be destroyed
-                            Destroy(component);
+                            Object.Destroy(component);
                     }
                 }
             }
-    
+            
+            // destroys all components of game object except the list of those provided as method parameter
+            public static void DestroyComponentsExceptProvided(this GameObject gameObject, List<Component> componentsToKeep)
+            {
+                var allComponents = gameObject.GetComponents(typeof(Component));
+
+                foreach (var component in allComponents)
+                {
+                    if (component != null)
+                    {
+                        if (!componentsToKeep.Contains(component))
+                        {
+                            if (component.GetType() != typeof(Transform)) // Transform component cannot be destroyed
+                                Object.Destroy(component);
+                        }
+                    }
+                }
+            }
+
             // destroys all components of game object
-            public static void DestroyComponents(GameObject gameObject)
+            public static void DestroyAllComponents(this GameObject gameObject)
             {
                 var allComponents = gameObject.GetComponents(typeof(Component));
         
@@ -54,31 +81,21 @@ namespace Skallu.Utils
                     if (component != null)
                     {
                         if (component.GetType() != typeof(Transform)) // Transform component cannot be destroyed
-                            Destroy(component);
+                            Object.Destroy(component);
                     }
                 }
             }
-
-            // returns vector2 from angle provided as method parameter
-            public static Vector2 ReturnVectorFromAngle(float angle)
-            {
-                angle = (angle + 90f) * 0.0055555f * Mathf.PI;
-        
-                return new Vector2(Mathf.Sin(angle), Mathf.Cos(angle));
-            }
-
         }
 
         public static class StringExtensionUtils
         {
             // changes color of log message to one provided as method parameter
             //
-            // call example: Debug.Log("test".Color(Color.blue));
+            // call example: Debug.Log("sample text".Color(Color.blue));
             public static string Color(this string text, Color color)
             {
                 return $"<color=#{ColorUtility.ToHtmlStringRGB(color)}>{text}</color>";
             }
-            
         }
         
     }
