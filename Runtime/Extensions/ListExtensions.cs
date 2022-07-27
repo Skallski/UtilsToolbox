acc84af4cf0f967a8dd3b062ff5f9e86e411d1dd
Skallu <patryk.kaminski0711@gmail.com>
2022-07-28 01:51:25 +0200

    List extensions updated

--- Runtime/Extensions/ListExtensions.cs
+++ Runtime/Extensions/ListExtensions.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using UnityEngine;
 
 namespace SkalluUtils.Extensions
 {
@@ -13,8 +12,14 @@ namespace SkalluUtils.Extensions
             /// <param name="list"> list on which the method will be called </param>
             /// <typeparam name="T"> generic type of list </typeparam>
             /// <returns> random list item </returns>
-            public static T RandomItem<T>(this IList<T> list) => list[Random.Range(0, list.Count)];
-            
+            public static T RandomItem<T>(this IList<T> list)
+            {
+                if (list.Count == 0)
+                    throw new System.IndexOutOfRangeException("Cannot select random item from empty list!");
+
+                return list[UnityEngine.Random.Range(0, list.Count)];
+            }
+
             /// <summary>
             /// Shuffles list
             /// </summary>
@@ -24,11 +29,37 @@ namespace SkalluUtils.Extensions
             {
                 for (var i = list.Count - 1; i > 1; i--)
                 {
-                    var j = Random.Range(0, i + 1);
+                    var j = UnityEngine.Random.Range(0, i + 1);
                     var value = list[j];
                     list[j] = list[i];
                     list[i] = value;
                 }
+            }
+            
+            /// <summary>
+            /// Check if list contains all elements of second list
+            /// </summary>
+            /// <param name="a"> list on which the method will be called </param>
+            /// <param name="b"> list to check if "A" contains all of its elements </param>
+            /// <typeparam name="T"> generic type of list </typeparam>
+            /// <returns> true or false, depending on whether the list contains all elements of the second list </returns>
+            public static bool ContainsAll<T>(this IList<T> a, IEnumerable<T> b)
+            {
+                var contains = false;
+    
+                foreach (var element in b)
+                {
+                    if (a.Contains(element))
+                    {
+                        contains = true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+    
+                return contains;
             }
         }
     }
